edf94b1a3a67fe2db9beabed072b176b614be8d2
patrickmdey <patrickmdey@hotmail.com>
2023-08-30 16:42:07 -0300

    feat: ddl and requirements.txt

---
+++ countries_ddl.sql
@@ -0,0 +1,14 @@
+-- this would be the DDL for the database. I used it as a reference for the sqlalchemy models
+-- This has really no use
+CREATE DATABASE vemo_db;
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    flag VARCHAR(255) NOT NULL UNIQUE,
+    population INT NOT NULL,
+    currencies VARCHAR(255) NOT NULL,
+    languages VARCHAR(255) NOT NULL,
+    continents VARCHAR(255) NOT NULL,
+    capitals VARCHAR(255) NOT NULL
+);
